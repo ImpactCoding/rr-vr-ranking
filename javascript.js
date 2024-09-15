2934bb4dff27ed511aa82ab36f4616bec9cfec6d
ImpactCoding <impact.coding0430@gmail.com>
2024-09-15 14:58:45 +0200

    asdf

--- javascript.js
+++ javascript.js
@@ -39,7 +39,6 @@ const vueApp = {
           max_players: localStorage.getItem("max_players") || 100,
         },
       },
-      players: [],
       ranks: [
         {
           name: "Overall",
